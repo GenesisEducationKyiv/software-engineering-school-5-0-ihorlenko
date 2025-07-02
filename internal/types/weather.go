cd9dd32a8600aa69c0c29f104c30041c89c97bb5
Ihor <>
2025-07-02 15:14:57 +0300

    fix: add tag to validate city in weatherdata

--- internal/types/weather.go
+++ internal/types/weather.go
@@ -1,7 +1,7 @@
 package types
 
 type WeatherData struct {
-	City        string  `json:"city"`
+	City        string  `json:"city" validate:"required,min=1,max=200"`
 	Temperature float64 `json:"temperature"`
 	Humidity    float64 `json:"humidity"`
 	Description string  `json:"description"`
